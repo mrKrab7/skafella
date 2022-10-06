d87d4311b596e3118276fe9e3378cbc430002556
nikita <mr.vip.13@bk.ru>
2022-10-06 18:09:19 +0700

    секция send

--- lib2/slider-settings.js
+++ lib2/slider-settings.js
@@ -1,9 +1,13 @@
-var mainSwiper = new Swiper(".main-swiper", {
+let mainSwiper = new Swiper(".main-swiper", {
 
 
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
 
     loop: true,
@@ -22,4 +26,35 @@ var mainSwiper = new Swiper(".main-swiper", {
             });
         }
     }
+});
+
+let cardsSwiper = new Swiper(".projects__swiper", {
+
+
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: "#project__swiperNext",
+        prevEl: "#project__swiperPrev",
+    },
+
+    loop: true,
+    // autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false,
+    // },
+    // on: {
+    //     init() {
+    //         this.el.addEventListener('mouseenter', () => {
+    //             this.autoplay.stop();
+    //         });
+    //
+    //         this.el.addEventListener('mouseleave', () => {
+    //             this.autoplay.start();
+    //         });
+    //     }
+    // }
 });
